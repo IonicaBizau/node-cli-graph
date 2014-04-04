54ebd1bf679b6f39dc16ba139a1b7135e514fb30
Ionică Bizău <bizauionica@yahoo.com>
2014-04-04 13:42:14 +0300

    Added comments

--- index.js
+++ index.js
@@ -1,20 +1,65 @@
 /**
- * TODO
+ *  Function Graph
+ *  ==============
+ *  Easily draw function graphs via NodeJS in Terminal.
+ *
+ *  Example
+ *  =======
+ *
+ *     var FunctionGraph = require("function-graph");
+ *
+ *     function foo (x) {
+ *         return Math.sin(x) * 3;
+ *     }
+ *
+ *
+ *     var graph = new FunctionGraph ({
+ *         height: 30
+ *       , width: 50
+ *       , marks: {
+ *             hAxis: '─'
+ *           , vAxis: '│'
+ *           , center: '┼'
+ *           , point: '•'
+ *       }
+ *     });
+ *
+ *     for (var i = -25; i < 48; ++i) {
+ *         graph.addPoint(i, foo(i));
+ *     }
+ *
+ *     console.log(graph.toString());
+ *
+ *  Licensed under MIT license. See README file for more details.
  *
  */
 function FunctionGraph (options) {
 
+    // use `new` to create a new instance
+    if (this.constructor !== FunctionGraph) {
+        throw new Error ("Use new constructor to create a new instance of this function.");
+    }
+
+    // get instance
     var self = this;
+
+    // set an empty array for _graph field
     self._graph = [];
 
-    options.width = options.width || 60;
+    // merge defaults with options
+    options.width  = options.width || 60;
     options.height = options.height || 40;
-    options.center = options.center || {
-        x: Math.floor(options.width / 2)
-      , y: Math.floor(options.height / 2)
-    };
-    options.marks = options.marks;
 
+    // set the center of the graph
+    options.center = options.center || {
+        x: Math.floor (options.width  / 2)
+      , y: Math.floor (options.height / 2)
+    };
+
+    // set marks
+    options.marks = options.marks || {};
+
+    // these are the default marks
     defaultMarks = {
         hAxis: '-'
       , vAxis: '|'
@@ -24,10 +69,12 @@ function FunctionGraph (options) {
       , topArrow: "^"
     };
 
+    // merge defaults
     for (var mark in defaultMarks) {
         options.marks[mark] = options.marks[mark] || defaultMarks[mark];
     }
 
+    // build the graph using spaces
     for (var i = 0; i < options.height; ++i) {
         self._graph[i] = [];
         for (var ii = 0; ii < options.width; ++ii) {
