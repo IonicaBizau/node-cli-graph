fc908ef1f00c16d470fe34866aad3c7e9639edf8
Ionică Bizău <bizauionica@yahoo.com>
2015-05-11 15:19:41 +0300

    Updated the test

--- test/index.js
+++ test/index.js
@@ -13,15 +13,18 @@ it("should merge the options correctly", function (cb) {
 });
 
 it("should create the graph and stringify it", function (cb) {
-    var circle = new CliGraph({ height: 32, width: 32 }).setFunctionX(function (x) {
-        return Math.sqrt(200 - x * x);
-    }).setFunctionX(function (x) {
-        return -Math.sqrt(200 - x * x);
-    }).setFunctionY(function (y) {
-        return Math.sqrt(200 - y * y);
-    }).setFunctionY(function (y) {
-        return -Math.sqrt(200 - y * y);
-    });
+    var radius = 49
+      , circle = new CliGraph({ height: 100, width: 100 }).setFunctionX(function (x) {
+            return Math.sqrt(radius * radius - x * x);
+        }).setFunctionX(function (x) {
+            return -Math.sqrt(radius * radius - x * x);
+        }).setFunctionY(function (y) {
+            return Math.sqrt(radius * radius - y * y);
+        }).setFunctionY(function (y) {
+            return -Math.sqrt(radius * radius - y * y);
+        })
+      ;
+
     Assert.equal(circle.toString(), CIRCLE);
     cb();
 });
